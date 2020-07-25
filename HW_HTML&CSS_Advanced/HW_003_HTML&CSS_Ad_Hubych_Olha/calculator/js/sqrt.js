a0c408c8ddb6f76c91a80298ccd9218715b51a4e
Olha Hubych <olya.gubich@gmail.com>
2020-07-25 12:07:49 +0300

    Fixed CSS-view for sqrt function

--- HW_HTML&CSS_Advanced/HW_003_HTML&CSS_Ad_Hubych_Olha/calculator/js/sqrt.js
+++ HW_HTML&CSS_Advanced/HW_003_HTML&CSS_Ad_Hubych_Olha/calculator/js/sqrt.js
@@ -2,7 +2,8 @@ addEventListener('message', function(e) {
     let sqrtResult;
     if(e.data == 0) sqrtResult = 0;
     else sqrtResult = Math.sqrt(e.data);
-
+    console.log(sqrtResult);
+    
     postMessage(sqrtResult);
 
 }, false);
